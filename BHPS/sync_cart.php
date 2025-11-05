410681773ce6690cfc181cd2cd97b707a3cb6038
Fakhrul Hilmi Bin Azman <108678876+thizsucc@users.noreply.github.com>
2025-11-05 21:07:43 +0800

    Update sync_cart.php to handle cart session
    Store cart data in session and return it in response.

--- BHPS/sync_cart.php
+++ BHPS/sync_cart.php
@@ -1,8 +1,11 @@
 <?php
 session_start();
-if (isset ($_POST['cart])) {
+if (isset($_POST['cart'])) {
     $cart = json_decode($_POST['cart'], true);
     if (is_array($cart)) {
+        $_SESSION['cart'] = $cart;
+        echo json_encode(['success' => true, 'cart' => $_SESSION['cart']]);
+        exit;
     }
 }
 echo json_encode(['success' => false]);
