191e82a5764f26e4c718f64a3043b71f91aa4c2c
Fakhrul Hilmi Bin Azman <108678876+thizsucc@users.noreply.github.com>
2025-11-05 21:06:53 +0800

    Remove cart session handling in sync_cart.php
    Refactor sync_cart.php to remove session storage logic.

--- BHPS/sync_cart.php
+++ BHPS/sync_cart.php
@@ -1,12 +1,8 @@
 <?php
 session_start();
-// Accept cart JSON via POST and store in session
-if (isset($_POST['cart'])) {
+if (isset ($_POST['cart])) {
     $cart = json_decode($_POST['cart'], true);
     if (is_array($cart)) {
-        $_SESSION['cart'] = $cart;
-        echo json_encode(['success' => true, 'cart' => $_SESSION['cart']]);
-        exit;
     }
 }
 echo json_encode(['success' => false]);
